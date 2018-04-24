0e1f33e73a93d9cf11c6c0fceeada0336d0321cf
李琦 <liqi@abrain.com.cn>
2018-04-24 09:59:08 +0800

    init config

--- serv_eureka/src/main/java/com/liquidli/ms/serv/eureka/ServEurekaApplication.java
+++ serv_eureka/src/main/java/com/liquidli/ms/serv/eureka/ServEurekaApplication.java
@@ -3,12 +3,21 @@ package com.liquidli.ms.serv.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class ServEurekaApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ServEurekaApplication.class, args);
+
+//		SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken("admin", "123",
+//				AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER")));
 	}
 }
