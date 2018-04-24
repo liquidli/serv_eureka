82d604083506bd21cbbaa30c195c4af370df074a
李琦 <liqi@abrain.com.cn>
2018-04-24 12:35:19 +0800

    init import

---
+++ ms_core_login/src/main/java/com/liquidli/ms/core/login/MsCoreLoginApplication.java
@@ -0,0 +1,14 @@
+package com.liquidli.ms.core.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MsCoreLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCoreLoginApplication.class, args);
+	}
+}
---
+++ ms_core_login/src/test/java/com/liquidli/ms/core/login/MsCoreLoginApplicationTests.java
@@ -0,0 +1,16 @@
+package com.liquidli.ms.core.login;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MsCoreLoginApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
